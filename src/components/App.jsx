b1ca62716e71370c7314114e6cf0014663c65acc
Oleh Protasevych <protasevicholeg@gmail.com>
2023-05-26 23:38:21 +0300

    Done 1st part of 2nd Homework

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// App.jsx
+
+import React from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import Layout from './layout';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = type => {
+    this.setState(currentState => ({ [type]: currentState[type] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  renderStatistics = () => {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return totalFeedback === 0 ? (
+      <Notification message="There is no feedback" />
+    ) : (
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={totalFeedback}
+        positivePercentage={positivePercentage}
+      />
+    );
+  };
+
+  render() {
+    const options = ['good', 'neutral', 'bad'];
+
+    return (
+      <Layout>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        <Section title="Statistics">{this.renderStatistics()}</Section>
+      </Layout>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+import App from 'components/App';
 import './index.css';
+
+const theme = {
+  colors: {
+    statListMainBgColor: '#e6ebf0',
+    statListEvenBgColor: '#F3F6F5',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
